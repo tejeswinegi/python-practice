0e042dd8c266f257e51939ff62b95e7d773a88c4
tejeswinegi <60092435+tejeswinegi@users.noreply.github.com>
2020-03-28 15:27:00 +0530

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name = 'Tejeswi Negi'
+my_age = 21 #Time to get married
+my_height = 170 #if not more
+my_weight = 96 # kgs, i'm fat as fuck
+my_eyes ='Black'
+my_teeth = 'Yellow'
+my_hair = 'Black'
+
+print " Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d Kilo heavy." % my_weight
+print "Actually that's not too heavy.I'm just kidding"
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth 
+
